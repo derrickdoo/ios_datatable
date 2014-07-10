da8855c32d332b63a4b1d63aa61dd994f76dc778
derricko <derricko@yahoo-inc.com>
2014-07-10 16:24:32 -0700

    Initial Commit

---
+++ tableExcercise1Tests/tableExcercise1Tests.m
@@ -0,0 +1,34 @@
+//
+//  tableExcercise1Tests.m
+//  tableExcercise1Tests
+//
+//  Created by Derrick Or on 7/10/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tableExcercise1Tests : XCTestCase
+
+@end
+
+@implementation tableExcercise1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
